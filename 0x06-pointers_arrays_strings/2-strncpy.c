344c29c6159b381ded1633742f96ac4460d33412
Successfuljohn <successfuljohn01@gmail.com>
2022-09-22 04:35:52 -0700

    C more pointers, 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcncpy - copies a string.
+ * @dest: destintion.
+ * @src: source.
+ * @n: bytes from src.
+ * Return: the pointer to dest.
+ */
+char *_strcncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
