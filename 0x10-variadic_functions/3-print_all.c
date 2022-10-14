ac78a626ef7cb14319d34b4fed2bfa348f7ba829
Successfuljohn <successfuljohn01@gmail.com>
2022-10-13 18:13:15 -0700

    C variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,6 +30,9 @@ void print_all(const char * const format, ...)
 			case 'c':
 			printf("%c", va_arg(valist, int)), c = 1;
 			break;
+			case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
 			case 'f':
 			printf("%f", va_arg(valist, double)), c = 1;
 			break;
